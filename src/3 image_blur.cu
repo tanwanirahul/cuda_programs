d53b5b503eaa43e804c33bf126e73fe6e78faacf
Rahul Tanwani <tanwanirahul@gmail.com>
2025-02-13 16:29:00 +0530

    Add Time profiling for ImageBlur

--- src/3 image_blur.cu
+++ src/3 image_blur.cu
@@ -1,6 +1,8 @@
+%%writefile image_blur.cu
 
 #include "stdlib.h"
 #include "img_utils.h"
+#include "timer.h"
 
 __global__ void image_blur_kernel(unsigned char * original, unsigned char * blur, unsigned int blur_size, unsigned int width, unsigned int height) {
 
@@ -18,7 +20,7 @@ __global__ void image_blur_kernel(unsigned char * original, unsigned char * blur
                 }
             }
         }
-        blur[row*width+col] = (unsigned char) average / ((2 * blur_size + 1) * (2 * blur_size + 1)); 
+      blur[row*width+col] = (unsigned char) (average / ((2 * blur_size + 1) * (2 * blur_size + 1))); 
     }
 }
 
@@ -28,21 +30,38 @@ void image_blur_wrapper(unsigned char * original, unsigned char * blur, unsigned
     unsigned char *original_d, *blur_d;
     size_t image_size = sizeof(unsigned char) * width * height;
 
+    Timer timer;
+    timer = initTimer(1);
+    startTimer(&timer);
+
     cudaMalloc((void **) &original_d, image_size);
     cudaMalloc((void **) &blur_d, image_size);
+    cudaDeviceSynchronize();
+    stopAndPrintElapsed(&timer, "GPU Device Memory Allocation Time: ", CYAN);
+
 
     // Copy data from Host to GPU.
+    timer = initTimer(1);
+    startTimer(&timer);
     cudaMemcpy(original_d, original, image_size, cudaMemcpyHostToDevice);
     cudaDeviceSynchronize();
+    stopAndPrintElapsed(&timer, "Time To Copy Data to GPU DRAM: ", CYAN);
 
     // Do the computation on the device.
+    timer = initTimer(1);
+    startTimer(&timer);
     dim3 threadsPerBlock(32, 32);
     dim3 numBlocks((width + threadsPerBlock.x - 1) / threadsPerBlock.x, (height + threadsPerBlock.y - 1) / threadsPerBlock.y ); 
     image_blur_kernel<<<numBlocks, threadsPerBlock>>>(original_d, blur_d, blur_size, width, height);
     cudaDeviceSynchronize();
+    stopAndPrintElapsed(&timer, "CUDA Kernel Execution Time: ", GREEN);
 
     // Copy the results back from GPU  to Host.
+    timer = initTimer(1);
+    startTimer(&timer);
     cudaMemcpy(blur, blur_d, image_size, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    stopAndPrintElapsed(&timer, "Time to COPY Results from GPU to HOST: ", CYAN);
 
     // Deallocate memory from the GPU device.
     cudaFree(original_d);
@@ -50,8 +69,8 @@ void image_blur_wrapper(unsigned char * original, unsigned char * blur, unsigned
 }
 
 int main(int argc, char** argv) {
-    unsigned int width = 1024;
-    unsigned int height = 1024;
+    unsigned int width = 10240;
+    unsigned int height = 10240;
     unsigned int blur_size = 5;
 
     size_t image_size = sizeof(unsigned char) * width * height;
@@ -78,16 +97,15 @@ int main(int argc, char** argv) {
     // Save the blur image;
     write_pgm_image(&blur_image, "blur_grey_image.ppm");
 
-    // printf("First 100 elements of the orignal and blur image:\n");
-    // int limit = (height * width) > 100? 100: (height * width);
-    // for(int i=0; i<limit; i++) {
-    //   printf("%d: %d       %d\n", i, image.grey[i], blur[i]);
-    // }
+    //printf("First 100 elements of the orignal and blur image:\n");
+    //int limit = (height * width) > width? width: (height * width);
+    //for(int i=0; i<limit; i++) {
+    //  printf("%d: %d       %d\n", i, image.grey[i], blur[i]);
+    //}
 
     // Free up allocated space on the host for original and blur image.
     unload_pgm_image(&image);
     free(blur);
 
     return 0;
-    
 }
